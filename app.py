7afa94380bd47ab2e01e922049aa793e768e1423
edurodrigues0 <edusacramento9@gmail.com>
2025-06-26 10:50:56 -0300

    Add logout functionality and update imports in Flask app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from models.users import User
 
 from database import db
 from flask import Flask, request, jsonify
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissecret'
@@ -34,9 +34,16 @@ def authenticate():
       
   return jsonify({"message": "Invalid credentials"}), 400
 
+@app.route("/logout", methods=['GET'])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({"message": "Logged out"}), 200
+    
+
 @app.route("/hello-world", methods=['GET'])
 def hello_world():
   return "Hello World"
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True, port=3333)
