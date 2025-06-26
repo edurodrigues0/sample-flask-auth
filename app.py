22ea2b4ccedea38dcd93b186a8bf76a81f006b1f
edurodrigues0 <edusacramento9@gmail.com>
2025-06-25 21:00:34 -0300

    Refactor database initialization and user model import structure

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.users import User
+from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 @app.route("/hello-world", methods=['GET'])
 def hello_world():
--- models/users.py
+++ models/users.py
@@ -1,11 +1,13 @@
-from app import db
 import datetime
 import uuid
 
-class User(db.Model):
-  id = db.Column(db.String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
-  name = db.Column(db.String(80), nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(), nullable=False)
-  created_at = db.Column(db.Datetime, nullable=False, default=datetime.utcnow)
-  updated_at = db.Column(db.Datetime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+from database import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.String(36), primary_key=True, unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.timezone.utc)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.timezone.utc, onupdate=datetime.datetime.utcnow)
