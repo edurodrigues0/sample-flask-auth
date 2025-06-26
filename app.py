0fc05194ab219f28b4c9e0dac052bc5ffe36a961
edurodrigues0 <edusacramento9@gmail.com>
2025-06-25 21:56:33 -0300

    Refactor user authentication logic and update User model timestamps

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from models.users import User
 
 from database import db
-from flask_login import LoginManager
 from flask import Flask, request, jsonify
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissecret'
@@ -12,8 +12,12 @@ login_manager = LoginManager()
 
 db.init_app(app)
 login_manager.init_app(app)
-# view login
 
+login_manager.login_view = 'authenticate'
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.filter(User.id == user_id).first()
 
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
@@ -25,6 +29,7 @@ def authenticate():
     user = User.query.filter(User.email == email).first()
 
     if user and user.password == password:
+      login_user(user)
       return jsonify({"message": "Authenticated"}), 200
       
   return jsonify({"message": "Invalid credentials"}), 400
--- models/users.py
+++ models/users.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timezone
 import uuid
 
 from database import db
@@ -9,5 +9,5 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.timezone.utc)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.timezone.utc, onupdate=datetime.datetime.utcnow)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
